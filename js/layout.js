5289947c6d504a1abebe829cedf3bf0aa87667d5
Ivan Renteria <30631018+Ivannov87@users.noreply.github.com>
2020-05-30 19:44:17 -0600

    update js files
    update

--- js/layout.js
+++ js/layout.js
@@ -148,7 +148,7 @@ var Layout = function () {
             });
         },
 
-        // To get the correct viewport width based on  http://andylangton.co.uk/articles/javascript/get-viewport-size-javascript/
+
         getViewPort: function() {
             var e = window,
                 a = 'inner';
